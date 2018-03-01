a2105716ffb4e4559c17ffaa13e154651524c86c
returnlytom <tom@returnly.com>
2018-02-28 22:55:21 -0800

    Fix Mixpanel syntax

--- lib/event_tracker/jobs/mixpanel_job.rb
+++ lib/event_tracker/jobs/mixpanel_job.rb
@@ -4,7 +4,7 @@ module EventTracker
   module Jobs
     class MixpanelJob < TrackerJob
       def perform(doer_id, event_name, event_label, properties)
-        tracker.track(doer_id, event_name, event_label, properties)
+        tracker.track(doer_id, event_label, properties.merge(namespaced: event_name))
       end
 
       private
